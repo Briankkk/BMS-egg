857c3b8818557dca2638a23dcb88000325d894e3
brian <13813009693@139.com>
2018-06-08 16:34:56 +0800

    分页查询

--- app/service/customer.js
+++ app/service/customer.js
@@ -3,7 +3,18 @@ const Service = require('egg').Service;
 class CustomerService extends Service {
     async index() {
         const client_global = this.app.mysql.get('db_global');
-        const customers = await client_global.select('customer',{CUST_ID:this.ctx.session.staff.CUST_ID});
+        //console.log(this.ctx.query.PAGE_SIZE)
+        //console.log(this.ctx.query.PAGE_INDEX)
+        const PAGE_SIZE = parseInt(this.ctx.query.PAGE_SIZE);
+        const PAGE_INDEX = parseInt(this.ctx.query.PAGE_INDEX);
+        const customers = await client_global.select('customer',{
+            //CUST_ID:this.ctx.session.staff.CUST_ID,
+            where: { CUST_ID: this.ctx.session.staff.CUST_ID},
+            //columns: ['author', 'title'], // 要查询的表字段
+            //orders: [['created_at','desc'], ['id','desc']], // 排序方式
+            limit: PAGE_SIZE ,
+            offset: (PAGE_INDEX - 1) * PAGE_SIZE,
+        });
         return customers;
     }
 
